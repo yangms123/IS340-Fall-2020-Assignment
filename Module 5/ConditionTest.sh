b1230d0cdf46c6ac553c45ba9c0a04b4a85ebe6d
Ubuntu <ubuntu@ip-172-31-1-184.us-east-2.compute.internal>
2020-11-09 07:33:54 +0000

    Submission for Module 5 Mingsheng Yang

---
+++ Module 5/ConditionTest.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+printf "How many pizzas do you want to buy?\n"
+read amount
+if (( amount <=0 ))
+then 
+	printf "Please enter a number that is greater than 0.\n"
+elif(( amount >10 ))
+then 
+	printf "Sorry, we do not have that many pizzas.\n"
+	exit 1
+else
+	printf "There you go. Here are %d pizzas for you.\n" "$amount"
+fi
